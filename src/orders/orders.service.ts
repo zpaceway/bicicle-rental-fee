3fc57b7d7aa885109c4f468dc53cd664bca8ba52
and_95yam <andy_yamada@hotmail.com>
2024-08-02 18:43:54 -0500

    prueba Karina Yanez

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -11,5 +11,45 @@ export class OrdersService {
     private orderRepository: Repository<Order>,
   ) {}
 
-  async processOrder(processOrderDto: ProcessOrderDto) {}
+  async processOrder(processOrderDto: ProcessOrderDto) {
+   
+      let rentalFee = 0;
+  
+      if (processOrderDto.hours <= 1) {
+        rentalFee = 2;
+      } else if (processOrderDto.hours <= 3) {
+        rentalFee = 5;
+      }
+      else if (processOrderDto.hours <= 6) {
+        rentalFee = 9;
+      }
+      else if (processOrderDto.hours <= 12) {
+        rentalFee = 15;
+      }
+       else {
+         throw new Error('Unprocessable entry');
+      }
+  
+      // registrar
+  
+      const orderDataForInsert = {
+         id: processOrderDto.orderId,
+         userId: processOrderDto.userId,
+         hours: processOrderDto.hours,
+         rentalFee: rentalFee        
+      };      
+  
+      let order = this.orderRepository.create(orderDataForInsert);
+      await this.orderRepository.save(order);
+  
+      if (!order) {
+         throw new Error('Unprocessable entry');
+      }
+  
+      console.log("Order create with id: " + order.id);
+  
+      return order;
+  
+   
+  }
 }
