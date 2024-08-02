d17676ce013df19991ec9b80accb763857f95f98
= <=>
2024-08-02 18:23:39 -0500

    Subida de prueba

--- src/entities/order.entity.ts
+++ src/entities/order.entity.ts
@@ -14,4 +14,30 @@ export class Order {
 
   @Column('float')
   rentalFee: number;
+
+  private rentaBicicleta = [
+    [1, 2],
+    [3, 5],
+    [6, 9],
+    [12, 15],
+  ];
+
+  getRenta() {
+    for (const [hoursBici, rentaDolares] of this.rentaBicicleta) {
+      if (this.hours <= hoursBici) {
+        return rentaDolares;
+      }
+    }
+
+    return null;
+  }
+
+  calcularRenta() {
+    const renta = this.getRenta();
+    if (renta === null) {
+      throw new Error('Unprocessable entry');
+    }
+
+    this.rentalFee = renta;
+  }
 }
