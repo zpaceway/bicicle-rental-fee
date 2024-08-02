f0fafffc87f7bbc829bd953290a5f6f608b7d004
Mauricio89 <edison_m_11@hotmail.com>
2024-08-02 18:25:56 -0500

    prueba

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -11,5 +11,21 @@ export class OrdersService {
     private orderRepository: Repository<Order>,
   ) {}
 
-  async processOrder(processOrderDto: ProcessOrderDto) {}
+  async processOrder(processOrderDto: ProcessOrderDto) {
+    const order = this.orderRepository.create({
+      id: processOrderDto.orderId,
+      userId: processOrderDto.userId,
+      hours: processOrderDto.hours,
+    });
+
+    order.calcularAlquilar();
+    await this.orderRepository.insert(order);
+
+    return {
+      id: processOrderDto.orderId,
+      userId: processOrderDto.userId,
+      hours: processOrderDto.hours,
+      rentalFee: order.rentalFee,
+    };
+  }
 }
