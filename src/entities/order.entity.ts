2a89b7a0ff0f7b5c68c5dc3e626e2b00be9ae81a
hcenteno <centenohd@hotmail.com>
2024-08-02 18:41:13 -0500

    Hernan Centeno

--- src/entities/order.entity.ts
+++ src/entities/order.entity.ts
@@ -2,6 +2,14 @@ import { Entity, Column, PrimaryColumn, Index } from 'typeorm';
 
 @Entity()
 export class Order {
+
+  private horaCosto = [
+    [1, 2],
+    [3, 5],
+    [6, 9],
+    [12, 15],
+  ];
+
   @PrimaryColumn('uuid')
   id: string;
 
@@ -14,4 +22,23 @@ export class Order {
 
   @Column('float')
   rentalFee: number;
+
+  
+  getAlquiler() {
+    for (const [horaAlquiler, precioAlquiler] of this.horaCosto) {
+      if (this.hours <= horaAlquiler && horaAlquiler < 13) {
+        return precioAlquiler;
+      }
+    }
+    return null;
+  }
+
+  calcularAlquilar() {
+    const rentalFee = this.getAlquiler();
+    if (rentalFee === null) {
+      throw new Error('Unprocessable entry');
+    }
+    this.rentalFee = rentalFee;
+  }
+
 }
