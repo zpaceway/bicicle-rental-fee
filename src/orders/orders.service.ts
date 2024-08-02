166aa32d090ce462f307c5034949c398ea9e0594
maryortiz93 <mariela.ortiz@espoch.edu.ec>
2024-08-02 18:36:03 -0500

    prueba Laura Ortiz

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -11,5 +11,39 @@ export class OrdersService {
     private orderRepository: Repository<Order>,
   ) {}
 
-  async processOrder(processOrderDto: ProcessOrderDto) {}
+  async processOrder(processOrderDto: ProcessOrderDto) {
+    let rentalfee = 0;
+
+    if (processOrderDto.hours <= 1) {
+      rentalfee = 2;
+    } else if (processOrderDto.hours <= 3) {
+      rentalfee = 5;
+    } else if (processOrderDto.hours <= 6) {
+      rentalfee = 9;
+    } else if (processOrderDto.hours <= 12) {
+      rentalfee = 15;
+    } else {
+      throw new Error('Unprocessable entry');
+    }
+
+    // registrar
+
+    const orderDataForInsert = {
+      id: processOrderDto.orderId,
+      userId: processOrderDto.userId,
+      hours: processOrderDto.hours,
+      rentalFee: rentalfee,
+    };
+
+    let order = this.orderRepository.create(orderDataForInsert);
+    await this.orderRepository.save(order);
+
+    if (!order) {
+      throw new Error('Unprocessable entry');
+    }
+
+    console.log('Order create with id: ' + order.id);
+
+    return order;
+  }
 }
